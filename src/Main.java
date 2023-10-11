d34d3e8390eca8f8d322d809471134908414ce19
Guo Feng He <guofengh@nycstudents.net>
2023-10-10 20:46:59 -0400

    added test cases

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args){
         CustomStringMethods methods = new CustomStringMethods();
-        String str = methods.starBetween("apple");
-        System.out.println(str);
-        str = methods.starBetween("Hey what's up?");
-        System.out.println(str);
-
+        System.out.println(methods.longerThan("cheese", 6));
+        System.out.println(methods.funnyString("bacon", 0));
+        System.out.println(methods.halvesReversed("grapes"));
+        System.out.println(methods.pigLatin("jason"));
+        System.out.println(methods.removeCharacter("gfuel",0));
+        System.out.println(methods.insertAt("bacon and es", "gg", "s"));
+        System.out.println(methods.endUp("guo feng", 4));
+        System.out.println(methods.yellOrWhisper("Ap csp"));
+        System.out.println(methods.starBetween("star"));
     }
 }
